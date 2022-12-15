18ec850c44b172cba9c64fdb3c5d84053f154012
Nick Landas <nick.landas@gmail.com>
2022-12-14 19:54:01 -0600

    commit tree 9b6b4187d166060b04487e5cba1be9f99826d52c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+    if array.length == 1
+        array.join
+    elsif array.length == 2 
+        array.join(' and ')
+    elsif array.length == 3
+        "#{array[0..1].join(', ')}, and #{array[-1]}"
+    elsif array.length > 3
+        "#{array[0...-1].join(', ')}, and #{array[-1]}"
+      end
 end
+
